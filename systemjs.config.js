4ba3d92ae46190164028b517e5378eea966ecdc4
Кобычева Алёна <kobycheva@adv.ru>
2017-04-15 13:37:43 +0800

    + include lodash

--- systemjs.config.js
+++ systemjs.config.js
@@ -25,6 +25,7 @@
 
       // other libraries
       'rxjs':                      'npm:rxjs',
+      'lodash':                         'npm:lodash',
       'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js'
     },
     // packages tells the System loader how to load when no filename and/or no extension
@@ -34,6 +35,9 @@
         defaultExtension: 'js'
       },
       rxjs: {
+        defaultExtension: 'js'
+      },
+      lodash: {
         defaultExtension: 'js'
       }
     }
